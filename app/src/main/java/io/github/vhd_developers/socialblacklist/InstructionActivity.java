3119e9c195949bfff3cef280454f3bb0604fe492
suhu3515 <suhu3515@gmail.com>
2020-06-15 20:54:51 +0530

    DAY 1 (15 JUNE 2020) ====================== Done splash screen ====================== TODO ====================== todo -> Change font of app name todo -> Add instructions in InstructionActivity

---
+++ app/src/main/java/io/github/vhd_developers/socialblacklist/InstructionActivity.java
@@ -0,0 +1,14 @@
+package io.github.vhd_developers.socialblacklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+    }
+}
